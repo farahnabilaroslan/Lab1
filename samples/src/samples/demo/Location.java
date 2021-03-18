0ada69d30c835208e0c45faa4cd7f380b7c56958
Farah Nabila Roslan <farahnabila.bella99@gmail.com>
2021-03-18 21:43:41 +0800

    Initial commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,11 @@
+package samples.demo;
+
+public class Location {
+
+	public static void main(String[] args) {
+		System.out.print("Location unknown");
+		// TODO Auto-generated method stub
+
+	}
+
+}
